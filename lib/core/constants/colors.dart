67b0ecbea179315ec73eb7caf18ebd385ebef1ec
Nikita Banskota <nikitabanskota2@gmail.com>
2026-07-02 19:57:00 +0545

    Worker-Dashboard

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  static const primary=Color(0xFF5A31F4);
+
+  static const secondary=Color(0xFF7B61FF);
+
+  static const background=Color(0xFFF9F9FC);
+
+  static const card=Color(0xFFFFFFFF);
+
+  static const green=Color(0xFF22C55E);
+
+  static const red=Color(0xFFFF3B30);
+
+  static const orange=Color(0xFFFF8C1A);
+
+  static const lightPurple=Color(0xFFF4F0FF);
+
+  static const textDark=Color(0xFF14142B);
+
+  static const textLight=Color(0xFF6E7191);
+
+  static const border=Color(0xFFEAEAEA);
+
+}
---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,18 @@
+class AppRoutes{
+
+  static const technicianHome=
+      '/technician-home';
+
+  static const jobs=
+      '/jobs';
+
+  static const earnings=
+      '/earnings';
+
+  static const profile=
+      '/profile';
+
+  static const requests=
+      '/requests';
+
+}
