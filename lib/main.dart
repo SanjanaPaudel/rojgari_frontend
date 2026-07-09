fb99efb3a25e9a59de20b6dc05089fded9a95594
Nikita Banskota <nikita@example.com>
2026-07-09 22:00:37 +0545

    Update splash screen UI and animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,24 @@
+// import 'package:flutter/material.dart';
+// import 'screens/welcome/welcome_screen.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: WelcomeScreen(),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
-import 'screens/welcome/welcome_screen.dart';
+import 'screens/splash/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +31,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: WelcomeScreen(),
+      home: SplashScreen(),
     );
   }
 }
